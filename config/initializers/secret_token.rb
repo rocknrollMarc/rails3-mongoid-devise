eee1232e87be0e785842a0b40cbf9c10a46b2fa0
rocknrollMarc <marc.bluemner@gmail.com>
2014-01-15 09:33:31 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails3MongoidDevise::Application.config.secret_key_base = 'c264b49547be7eed489f2b75457dd10ccad0c01ee5322c25888472b59aaef7291653d96419426a25e4a0148cf042628f15cec84b16b9ef6ba1658c01e22d225b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails3MongoidDevise::Application.config.session_store :cookie_store, key: '_rails3-mongoid-devise_session'
